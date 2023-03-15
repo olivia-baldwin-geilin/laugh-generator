e75c4cfc9745e7871c8beef5042ee0b1d33339a0
Olivia Baldwin-Geilin <olivia.baldwin-geilin@paramount.com>
2023-03-15 11:44:35 -0600

    added react front-end

---
+++ web-ui/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import './App.css';
+import Button from '@mui/material/Button'
+
+function App() {
+  const [joke, setJoke] = useState("No Joke Yet")
+
+  const get_joke = async () => {
+      fetch('http://0.0.0.0:5000/random_joke', {
+          method: 'GET'
+      })
+      .then((response) => response.json())
+      .then((data) => {
+          console.log('Successs: ', data);
+          setJoke(data.body)
+      })
+      .catch((error) => {
+          console.error('Error: ', error);
+      });
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="joke-button">
+          <Button onClick={get_joke} color="secondary" variant="contained">Make Me Laugh</Button>
+        </div>
+        <div className="joke">
+            {joke}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
